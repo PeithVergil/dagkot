c9de75804076e77c7b432fb4524ccec43471d095
Peith Vergil <peith.vergil@gmail.com>
2012-08-02 07:31:20 +0800

    * Added a login/logout link on the navbar.

--- base.py
+++ base.py
@@ -1,3 +1,5 @@
+from google.appengine.api import users
+
 from webapp2 import cached_property
 from webapp2 import RequestHandler
 from webapp2_extras import jinja2
@@ -12,6 +14,18 @@ class BaseRequestHandler(RequestHandler):
         return jinja2.get_jinja2(app=self.app)
         
     def render_html(self, template, **params):
+        user = users.get_current_user()
+        if user:
+            params['auth_url'] = users.create_logout_url('/')
+            params['auth_txt'] = 'Logout'
+            
+            params['nickname'] = user.nickname()
+        else:
+            params['auth_url'] = users.create_login_url('/')
+            params['auth_txt'] = 'Login'
+        params['user'] = user
+        
         tpl = self.jinja.render_template(template, **params)
+        
         self.response.write(tpl)
         
