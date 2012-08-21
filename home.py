63a2f5218fa960fec5b727e9e433f89288600383
Peith Vergil <peith.vergil@gmail.com>
2012-08-21 22:50:15 +0800

    Continue with the infinite-scroll integration. Fixing issues where the fetching of new dagkots during scrolling is not quite right.

--- home.py
+++ home.py
@@ -6,18 +6,17 @@ from models.dagkot import Dagkot
         
 class Home(base.BaseRequestHandler):
     def get(self):
-        data = {}
-
-        limit = 10
+        dagkots = Dagkot.all()
         offset = 0
+        limit = 10
 
         user = users.get_current_user()
         if user:
-            data['dagkots'] = Dagkot.all().filter('dagkot_author =', user).order('-dagkot_date').run(offset=offset, limit=limit)
+            dagkots = dagkots.filter('dagkot_author =', user).order('dagkot_date').run(offset=offset, limit=limit)
         else:
-            data['dagkots'] = Dagkot.all().order('-dagkot_date').run(offset=offset, limit=limit)
+			dagkots = dagkots.order('dagkot_date').run(offset=offset, limit=limit)
         
-        self.render_html('home/home.html', **data)
+        self.render_html('home/home.html', dagkots=dagkots)
 
 class Dagkots(base.BaseRequestHandler):
 	def get(self):
@@ -29,17 +28,17 @@ class Dagkots(base.BaseRequestHandler):
 		try:
 			offset = int(self.request.GET['o'])
 
-			if offset > 1:
-				offset = (offset - 1) * limit
-			else:
-				offset = 0
+			if offset > 0:
+				offset -= 1
+
+			offset *= limit
 		except KeyError:
 			offset = 0
 
 		user = users.get_current_user()
 		if user:
-		    dagkots = Dagkot.all().filter('dagkot_author =', user).order('-dagkot_date').run(offset=offset, limit=limit)
+		    dagkots = Dagkot.all().filter('dagkot_author =', user).order('dagkot_date').run(offset=offset, limit=limit)
 		else:
-		    dagkots = Dagkot.all().order('-dagkot_date').run(offset=offset, limit=limit)
+		    dagkots = Dagkot.all().order('dagkot_date').run(offset=offset, limit=limit)
 
 		self.render_html('home/dagkots.html', dagkots=dagkots)
