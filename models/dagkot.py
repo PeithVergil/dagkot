cb7147caab906927962f0c2552071f30a93c5a15
Peith Vergil <peith.vergil@gmail.com>
2012-08-23 21:36:36 +0800

    Added the get_timesince method in the Dagkot model.

--- models/dagkot.py
+++ models/dagkot.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from google.appengine.api import images
 from google.appengine.ext import db
 
@@ -29,6 +31,30 @@ class Dagkot(db.Model):
                 yield images.get_serving_url(picture, 205, True)
             else:
                 yield images.get_serving_url(picture, 48, True)
+
+    def get_timesince(self):
+        """
+        Returns string representing "time since" e.g.
+        3 days ago, 5 hours ago etc.
+        """
+        now = datetime.utcnow()
+        diff = now - self.dagkot_date
+        
+        periods = (
+            (diff.days / 365, "year", "years"),
+            (diff.days / 30, "month", "months"),
+            (diff.days / 7, "week", "weeks"),
+            (diff.days, "day", "days"),
+            (diff.seconds / 3600, "hour", "hours"),
+            (diff.seconds / 60, "minute", "minutes"),
+            (diff.seconds, "second", "seconds"),
+        )
+
+        for period, singular, plural in periods:
+            if period:
+                return "%d %s ago" % (period, singular if period == 1 else plural)
+
+        return "just now"
 
 class Photo(db.Model):
     photo_key = db.StringProperty()
