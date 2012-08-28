5b0c0ee14b691296b17547a1a7f7e36ca89fdc1d
Peith Vergil <peith.vergil@gmail.com>
2012-08-29 01:14:32 +0800

    Added the "All Dagkots" and "My Dagkots" page.

--- dagkot.py
+++ dagkot.py
@@ -1,6 +1,8 @@
+from google.appengine.api import users
 from google.appengine.ext.db import BadKeyError
 
 import base
+import settings
 from models.dagkot import Dagkot
 
 class View(base.BaseRequestHandler):
@@ -24,3 +26,70 @@ class View(base.BaseRequestHandler):
 			self.render_html('home/landing.html',
 				message_content=message_cont,
 				message_type=message_type)
+
+class ViewAll(base.BaseRequestHandler):
+	def get(self):
+		limit = settings.DAGKOTS_PER_PAGE
+		offset = 0
+		dagkots = Dagkot.all().order('-dagkot_date').run(offset=offset, limit=limit)
+
+		self.render_html('dagkot/view-all.html', dagkots=dagkots)
+
+class ViewAllNext(base.BaseRequestHandler):
+	def _query(self, page):
+		limit = settings.DAGKOTS_PER_PAGE
+
+		try:
+			offset = int(page)
+		except ValueError:
+			offset = 0
+
+		if offset > 0:
+			offset -= 1
+
+		offset *= limit
+
+		dagkots = Dagkot.all().order('-dagkot_date').run(offset=offset, limit=limit)
+
+		self.render_html('dagkot/view-all-next.html', dagkots=dagkots)
+
+	def get(self, page):
+		self._query(page)
+
+	def post(self, page):
+		self._query(page)
+
+class ViewMine(base.BaseRequestHandler):
+	def get(self):
+		user = users.get_current_user()
+		limit = settings.DAGKOTS_PER_PAGE
+		offset = 0
+		
+		dagkots = Dagkot.all().filter('dagkot_author =', user).order('-dagkot_date').run(offset=offset, limit=limit)
+
+		self.render_html('dagkot/view-mine.html', dagkots=dagkots)
+
+class ViewMineNext(base.BaseRequestHandler):
+	def _query(self, page):
+		user = users.get_current_user()
+		limit = settings.DAGKOTS_PER_PAGE
+
+		try:
+			offset = int(page)
+		except ValueError:
+			offset = 0
+
+		if offset > 0:
+			offset -= 1
+
+		offset *= limit
+
+		dagkots = Dagkot.all().filter('dagkot_author =', user).order('-dagkot_date').run(offset=offset, limit=limit)
+
+		self.render_html('dagkot/view-mine-next.html', dagkots=dagkots)
+
+	def get(self, page):
+		self._query(page)
+
+	def post(self, page):
+		self._query(page)
--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
 import webapp2
 
 routes = [
-	webapp2.Route('/'                              , handler='home.Home'           , name='home'),
 	webapp2.Route('/about'                         , handler='about.About'         , name='about'),
 	webapp2.Route('/add'                           , handler='add.Add'             , name='add'),
 	webapp2.Route('/add/error'                     , handler='add.AddError'        , name='add_error'),
+	webapp2.Route('/'                              , handler='dagkot.ViewAll'      , name='home'),
+	webapp2.Route('/next/<page:\d+>'               , handler='dagkot.ViewAllNext'  , name='home_next'),
+	webapp2.Route('/mine'                          , handler='dagkot.ViewMine'     , name='mine'),
+	webapp2.Route('/mine/next/<page:\d+>'          , handler='dagkot.ViewMineNext' , name='mine_next'),
 	webapp2.Route('/dagkots/get'                   , handler='home.Dagkots'        , name='dagkots_get'),
 	webapp2.Route('/upload/images/<dagkot_key:.+>' , handler='upload.UploadImages' , name='upload_images'),
 	webapp2.Route('/upload/handler/<dagkot_key:.+>', handler='upload.UploadHandler', name='upload_handler'),
